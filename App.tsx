0f54676a59e596020eb0231eb8bd26d5bba3968d
annguyen161 <nguyenducan.1601@gmail.com>
2025-03-25 00:44:06 +0700

    Implement Onboarding

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
-import React, {useEffect, useState} from 'react';
+import React, {use, useEffect, useState} from 'react';
 import AuthNavigator from './src/navigators/AuthNavigator';
 import {SplashScreen} from './src/screens';
 import {NavigationContainer} from '@react-navigation/native';
+import {StatusBar} from 'react-native';
+import {useAsyncStorage} from '@react-native-async-storage/async-storage';
+import MainNavigator from './src/navigators/MainNavigator';
 
 const App = () => {
   const [isShowSplash, setIsShowSplash] = useState(true);
+  const [accessToken, setAccessToken] = useState('');
+  const {getItem, setItem} = useAsyncStorage('assetToken');
+
+  const checkLogin = async () => {
+    const token = await getItem();
+
+    token && setAccessToken(token);
+  };
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -14,12 +29,22 @@ const App = () => {
     return () => clearTimeout(timeout);
   }, []);
 
-  return isShowSplash ? (
-    <SplashScreen />
-  ) : (
-    <NavigationContainer>
-      <AuthNavigator />
-    </NavigationContainer>
+  return (
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {isShowSplash ? (
+        <SplashScreen />
+      ) : (
+        <NavigationContainer>
+          {accessToken ? <MainNavigator /> : <AuthNavigator />}
+        </NavigationContainer>
+      )}
+      ;
+    </>
   );
 };
 
--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,8 +1,32 @@
-import {View, Text, ImageBackground} from 'react-native';
 import React from 'react';
+import {ActivityIndicator, Image, ImageBackground} from 'react-native';
+import {appInfo} from '../constants/appInfos';
+import {SpaceComponent} from '../components';
+import {appColors} from '../constants/appColors';
 
 const SplashScreen = () => {
-  return <View></View>;
+  return (
+    <ImageBackground
+      source={require('../assets/images/splash-img.png')}
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      imageStyle={{
+        flex: 1,
+      }}>
+      <Image
+        source={require('../assets/images/logo.png')}
+        style={{
+          width: appInfo.sizes.WIDTH * 0.7,
+          resizeMode: 'contain',
+        }}
+      />
+      <SpaceComponent height={20} />
+      <ActivityIndicator size={22} color={appColors.gray} />
+    </ImageBackground>
+  );
 };
 
 export default SplashScreen;
