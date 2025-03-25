ef1b28a2a50065e66b706573e2ca7240499d2ae4
annguyen161 <nguyenducan.1601@gmail.com>
2025-03-25 11:54:23 +0700

    Implement Add Icon

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,10 +1,16 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {fontFamilies} from '../../constants/fontFamilies';
+import {ButtonComponent} from '../../components';
+import {globalStyles} from '../../styles/globalStyles';
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={[globalStyles.container]}>
+      <Text style={{fontFamily: fontFamilies.regular, fontSize: 40}}>
+        LoginScreen
+      </Text>
+      <ButtonComponent disable text="SIGN IN" type="primary" />
     </View>
   );
 };
